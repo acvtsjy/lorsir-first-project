debf3e2d5903a3c5b54566894285029ff388661d
lorsir <lorsir@PC137.kyoceramita.it>
2022-09-01 15:21:45 +0200

    add gitignore

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello World!");
+console.log("This is my First Node Project!");
+console.log(":-D");
+let myvar = 3;
+myvar += 10;
+console.log("myvar = ", myvar);
--- main.ts
+++ main.ts
@@ -6,3 +6,16 @@ let myvar: number = 3;
 
 myvar += 10;
 console.log("myvar = ", myvar)
+
+ interface MyTnterface {
+    myNumber: number;
+    myString: string;
+ }
+
+ let myInterface: MyTnterface = {
+    myNumber: 10,
+    myString: 'hello World',
+ }
+
+ console.log("myNumber    = ", myInterface.myNumber);
+ console.log("myString = ", myInterface.myString);
